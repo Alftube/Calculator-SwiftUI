aefa50c47ed271d2a0dcbb57f42196748d51ad3f
Alf <66580888+Alftube@users.noreply.github.com>
2024-02-18 20:08:57 -0300

    Add files via upload

---
+++ Calculator/Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Alfredo Altube on 13/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
